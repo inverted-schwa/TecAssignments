052074db67de03c034164e0c579c74d1da050fe0
Owner <Owner@LAPTOP-08JICU6E>
2023-02-08 21:07:43 -0500

    added and fixed a bunch of things

--- src/magicOfBooks/MagicOfBooks.java
+++ src/magicOfBooks/MagicOfBooks.java
@@ -3,21 +3,59 @@ import java.util.*;
 
 public class MagicOfBooks {
 	static boolean isLoggedIn = false;
-	User[] userList=new User[4];
-	{
-	userList[0] = new User("Jim Jones", 1000, "jim.jones@gmail.com", "pass",null,null,null);
-	userList[1] = new User("Jane Jones", 1001, "jane.jones@gmail.com", "pass",null,null,null);
-	userList[2] = new User("Jill Jones", 1002, "Jill.jones@gmail.com", "pass",null,null,null);
-	userList[3] = new User("Jake Jones", 1003, "Jake.jones@gmail.com", "pass",null,null,null);
-	}
 	User current;
 	Book[] booklist = new Book[5];
 	{
 	booklist[0] = new Book("Bible", "God", "It's the bible!", 1);
 	booklist[1] = new Book("Fight Club", "Chuck Pahlaniuk", "It's Fight Club!", 2);
-	booklist[2] = new Book("History 101", "James Smith", "It's history!", 1);
-	booklist[3] = new Book("How to Do Stuff", "Edward McGill", "A convenient guide to life!", 1);
-	booklist[4] = new Book("How to Win Friends and Influence People", "Dale Carnegie", "The classic self-help book!", 1);
+	booklist[2] = new Book("History 101", "James Smith", "It's history!", 3);
+	booklist[3] = new Book("How to Do Stuff", "Edward McGill", "A convenient guide to life!", 4);
+	booklist[4] = new Book("How to Win Friends and Influence People", "Dale Carnegie", "The classic self-help book!", 5);
+	}
+	Book[] newBooksU1 = new Book[2];{
+	newBooksU1[0] = booklist[2];
+	newBooksU1[1] = booklist[3];
+	}
+	Book[] newBooksU2 = new Book[2];{
+		newBooksU2[0] = booklist[0];
+		newBooksU2[1] = booklist[4];
+	}
+	Book[] newBooksU3 = new Book[2];
+	{
+		newBooksU3[0] = booklist[1];
+		newBooksU3[1] = booklist[3];
+	}
+	Book[] newBooksU4 = new Book[2];
+	{
+		newBooksU4[0] = booklist[2];
+		newBooksU4[1] = booklist[4];
+	}
+	Book[] completedBooksU1 = new Book[2];
+	{
+		completedBooksU1[0] = booklist[1];
+		completedBooksU1[1] = booklist[2];
+	}
+	Book[] completedBooksU2 = new Book[2];
+	{
+		completedBooksU2[0] = booklist[3];
+		completedBooksU2[1] = booklist[1];
+	}
+	Book[] completedBooksU3 = new Book[2];
+	{
+		completedBooksU3[0] = booklist[4];
+		completedBooksU3[1] = booklist[1];
+	}
+	Book[] completedBooksU4 = new Book[2];
+	{
+		completedBooksU4[0] = booklist[3];
+		completedBooksU4[1] = booklist[2];
+	}
+	User[] userList=new User[4];
+	{
+	userList[0] = new User("Jim Jones", 1000, "jim.jones@gmail.com", "pass",newBooksU1,booklist[2],completedBooksU1);
+	userList[1] = new User("Jane Jones", 1001, "jane.jones@gmail.com", "pass",newBooksU2,booklist[4],completedBooksU2);
+	userList[2] = new User("Jill Jones", 1002, "Jill.jones@gmail.com", "pass",newBooksU3,booklist[3],completedBooksU3);
+	userList[3] = new User("Jake Jones", 1003, "Jake.jones@gmail.com", "pass",newBooksU4,booklist[0],completedBooksU4);
 	}
 	public Scanner sc;
 	
@@ -65,10 +103,11 @@ public class MagicOfBooks {
 		System.out.println("User: "+ this.current.userName +" Email: " + this.current.email);
 		System.out.println("1. Browse books");
 		System.out.println("2. Search for a book");
-		System.out.println("3. See completed books");
-		System.out.println("4. See favorite book");
-		System.out.println("5. List Users");
-		System.out.println("6. Quit");
+		System.out.println("3. See New Books");		
+		System.out.println("4. See completed books");
+		System.out.println("5. See favorite book");
+		System.out.println("6. List Users");
+		System.out.println("7. Quit");
 		
 int choice = sc.nextInt();
 		switch(choice) {
@@ -79,15 +118,18 @@ int choice = sc.nextInt();
 			this.searchBook();
 			break;
 		case 3:
-			this.listCompleted();
+			this.listNewBooks();
 			break;
 		case 4:
-			this.listFave();
+			this.listCompletedBooks();
 			break;
 		case 5:
-			this.listUsers();
+			this.listFave();
 			break;
 		case 6:
+			this.listUsers();
+			break;
+		case 7:
 			current=null;
 			isLoggedIn=false;
 			logIn();
@@ -125,11 +167,25 @@ int choice = sc.nextInt();
 		}
 	}
 	
-	public void listCompleted() {
-		
+	public void listNewBooks() {
+		for(Book b:this.current.newBooks)
+		{
+			System.out.println(b.bookName+" by "+b.authorName);
+			System.out.println(b.description);
+		}
+	}
+	
+	
+	public void listCompletedBooks() {
+		for(Book b:this.current.completed) {
+			System.out.println(b.bookName+" by "+b.authorName);
+			System.out.println(b.description);
+		}
 	}
 	
 	public void listFave() {
+
+		System.out.println(current.favorite.bookName+" by "+current.favorite.authorName+"/n "+current.favorite.description);
 		
 	}
 }
